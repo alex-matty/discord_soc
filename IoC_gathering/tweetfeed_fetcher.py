de9bdcf57ee4d2390ff7fb83ca8cd7bef1d5f9e5
alex-matty <alexmatty@protonmail.com>
2026-04-19 20:08:54 -0600

    First version of the tweetfeed IoC fetcher

---
+++ IoC_gathering/tweetfeed_fetcher.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+# Import required libraries
+import requests
+import json
+import re
+
+# Function to defang URLs or Domain names
+def defang(value, type_):
+  if type_ in ("ip", "domain"):
+    return value.replace(".", "[.]")
+  elif type_ == "url":
+    value = value.replace("http", "hxxp")
+    value = value.replace("://", "[://]")
+    value = value.replace(".", "[.]")
+    return value
+  return value
+
+# Build and send Chunked discord payloads
+def send_to_discord(items):
+  chunk = []
+  
+  for item in items:
+    chunk.append(item)
+
+    content = f"```json\n{json.dumps(chunk, indent=2)}\n```"
+    total_len = len(json.dumps({"content": content, "flags": 4}))
+
+    if total_len > CHAR_LIMIT:
+      chunk.pop()
+      flush_chunk(chunk)
+      chunk = [item]
+
+  if chunk:
+    flush_chunk(chunk)
+
+
+def truncate_content(content):
+  if len(content) > EFFECTIVE_LIMIT:
+    truncated = content[:EFFECTIVE_LIMIT -7]
+    return truncated + "\n...\n```"
+  return content
+
+def flush_chunk(chunk):
+  content = f"```json\n{json.dumps(chunk, indent=2)}\n```"
+  content = truncate_content(content)
+
+  discord_payload = {
+    "content": content,
+    "flags": 4
+  }
+
+  response = requests.post(WEBHOOK_URL, json=discord_payload)
+
+  print(f"status: {response.status_code}")
+  print(f"Response: {response.text}")
+  print(f"Content Length: {len(content)}")
+
+  response.raise_for_status()
+  print(f"Sent {len(chunk)} item(s) to Discord ({len(content)} chars)")
+
+# Tweetfeed API URL endpoint (@TODO Add variables to use filters)
+url = 'https://api.tweetfeed.live/v1/today'
+WEBHOOK_URL = "https://discord.com/api/webhooks/1495546244095934526/4ebkkg8PSdkID5nwIp5rFj6gRdpkLvhpnKC3x5TjK8bFWz27jGyfO4yvbMHG6yLVQUZ1"
+CHAR_LIMIT = 2000
+CODE_BLOCK = 15 
+DISCORD_PAYLOAD_OVERHEAD = 30
+EFFECTIVE_LIMIT = CHAR_LIMIT - DISCORD_PAYLOAD_OVERHEAD
+
+# Make the request to the API feed URL
+response = requests.get(url)
+
+# Parse JSON object
+data = response.json()
+
+json_payload = [
+  {
+    "date": item["date"],
+    "type": item["type"],
+    "value": defang(item["value"], item["type"]),
+    "tags": item["tags"],
+  }
+  for item in data
+]
+
+send_to_discord(json_payload)
